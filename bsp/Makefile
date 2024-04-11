716cb44385025b0e461e3d84d49b16080f361d3a
Mona Lisa <mona@github.com>
2024-04-12 01:38:32 +0800

    remove unused modules

--- bsp/Makefile
+++ bsp/Makefile
@@ -1,5 +1,3 @@
 # SPDX-License-Identifier: GPL-2.0
 
 obj-y += drivers/
-obj-y += modules/nand/
-obj-y += modules/gpu/
--- bsp/modules/gpu/img-rgx/linux/rogue_km/build/linux/config/compilers/mipsel-buildroot-linux-gnu.mk
+++
@@ -1,3 +0,0 @@
-# 32-bit MIPS compiler
-TARGET_PRIMARY_ARCH := target_mips32r6el
-SYSROOT ?= $(shell $(CROSS_COMPILE)gcc -print-sysroot)
