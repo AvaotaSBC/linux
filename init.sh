2b08bb93b0d2f75778d42e9a0e9c94fca9bb0cf0
SamulKyull <166363804+SamulKyull@users.noreply.github.com>
2024-04-12 09:43:18 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -100,6 +100,7 @@ echo Applying Patches and overlays
 cp -raf ${ROOT_PATH}/${PATCH_PATH}/${BRANCH}/*.patch .
 cp -raf ${ROOT_PATH}/${PATCH_PATH}/${BRANCH}/overlays/* .
 cp -raf ${ROOT_PATH}/.github .
+ls
 for patchfile in *.patch; do
     patch --binary -p1 < "$patchfile"
 done
